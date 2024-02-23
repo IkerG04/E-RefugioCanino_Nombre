b07e82cad2720731abf788a5450836a7ef03729b
IkerG04 <DAW110@IF03-12.iesmhp.local>
2024-02-23 18:49:24 +0100

    Añado perro llamdao elio

--- RefugioCanino/src/main/java/ed/refugiocanino/MainRefugioCanino.java
+++ RefugioCanino/src/main/java/ed/refugiocanino/MainRefugioCanino.java
@@ -13,11 +13,12 @@ public class MainRefugioCanino {
         // Agregar algunos perros al refugio
         refugio.agregarPerro("Max", "Labrador");
         refugio.agregarPerro("Bella", "Pastor Alemán");
-
+        refugio.agregarPerro("Elio", "Rojo");
         // Mostrar la lista de perros en el refugio
         System.out.println("\nLista de perros en el refugio:");
         refugio.mostrarPerros();
 
+        
         // Obtener y mostrar el número de perros en el refugio
         int numeroDePerros = refugio.obtenerNumeroDePerros();
         System.out.println("\nNúmero de perros en el refugio: " + numeroDePerros);
